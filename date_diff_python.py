723eef54a59f2e6eb1ec64590c7ec11b95648a7d
Gerardo <geryglez777@yahoo.es>
2020-10-24 16:26:04 -0400

    feat: Studying datetime conversion

---
+++ date_diff_python.py
@@ -0,0 +1,14 @@
+import datetime as dt
+import pandas as pd
+import time
+from time import time as tt
+
+start_date = '2020-01-01 23:30:00'
+end_date = '2020-01-02 00:15:00'
+
+test1 = start_date.replace('-',' ').replace(':',' ').replace(' ',', ')
+
+#def date_diff(start_date, end_date, value):
+
+
+print(test1)
